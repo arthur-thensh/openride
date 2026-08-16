beef57a0584a0d8d6f5eb8390fa3d89e69781d7b
arthur-thensh <9z5y6rjzbc@privaterelay.appleid.com>
2026-08-16 12:08:30 +0200

    Ride Planner: add shared planner mode

---
+++ include/openride/ride_planner.h
@@ -0,0 +1,9 @@
+#ifndef OPENRIDE_RIDE_PLANNER_H
+#define OPENRIDE_RIDE_PLANNER_H
+
+typedef enum OpenRideRidePlannerMode {
+    OPENRIDE_RIDE_PLANNER_ROUTE = 0,
+    OPENRIDE_RIDE_PLANNER_LOOP
+} OpenRideRidePlannerMode;
+
+#endif
